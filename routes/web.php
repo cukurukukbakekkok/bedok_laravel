6a9b7d466c3318780e10086cb637f6ef369c37bb
NizarCandra <nizarcandra0@gmail.com>
2025-08-12 14:45:40 +0700

    bedok

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('pages.beranda');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::view('contact','pages.contact');
